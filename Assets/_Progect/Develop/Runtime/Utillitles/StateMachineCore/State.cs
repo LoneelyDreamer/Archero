9ded63a80825d7f1b18556883cbf560b78c40dd3
emcha6work-arch <emcha6work@gmail.com>
2026-08-13 14:37:18 +0300

    IState

---
+++ Assets/_Progect/Develop/Runtime/Utillitles/StateMachineCore/State.cs
@@ -0,0 +1,19 @@
+﻿using Assets._Progect.Develop.Runtime.Utillitles.Reactivre;
+
+namespace Assets._Progect.Develop.Runtime.Utillitles.StateMachineCore
+{
+    public abstract class State : IState
+    {
+        private ReactiveEvent _entered = new();
+        private ReactiveEvent _exited = new();
+
+        public IReadOnlyEvent Entered => _entered;
+
+        public IReadOnlyEvent Exited => _exited;
+
+        public virtual void Enter() => _entered.Invoke();
+
+        public virtual void Exit() => _exited.Invoke();
+        
+    }
+}
---
+++ Assets/_Progect/Develop/Runtime/Utillitles/StateMachineCore/StateNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._Progect.Develop.Runtime.Utillitles.StateMachineCore
+{
+    public class StateNode<TState> where TState : class, IState
+    {
+        public StateNode(TState state)
+        {
+            State = state;
+        }
+
+        public TState State { get; }
+    }
+}
