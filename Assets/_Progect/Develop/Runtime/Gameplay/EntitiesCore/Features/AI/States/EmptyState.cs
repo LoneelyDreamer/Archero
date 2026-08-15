cc9df5d063e13377012b506384b6d1e62fbb4e3c
LoneelyDreamer <146751749+LoneelyDreamer@users.noreply.github.com>
2026-08-15 17:03:42 +0300

    Addede movement State

---
+++ Assets/_Progect/Develop/Runtime/Gameplay/EntitiesCore/Features/AI/States/EmptyState.cs
@@ -0,0 +1,9 @@
+﻿using Assets._Progect.Develop.Runtime.Utillitles.StateMachineCore;
+
+namespace Assets._Progect.Develop.Runtime.Gameplay.EntitiesCore.Features.AI.States
+{
+    public class EmptyState : State
+    {
+
+    }
+}
--- Assets/_Progect/Develop/Runtime/Utillitles/StateMachineCore/StateMachine.cs
+++ Assets/_Progect/Develop/Runtime/Utillitles/StateMachineCore/StateMachine.cs
@@ -1,10 +1,11 @@
 ﻿using Assets._Progect.Develop.Runtime.Utillitles.Conditions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Assets._Progect.Develop.Runtime.Utillitles.StateMachineCore
 {
-    public abstract class StateMachine<TState> where TState : class, IState
+    public abstract class StateMachine<TState> : IDisposable where TState : class, IState
     {
         private List<StateNode<TState>> _states = new();
 
@@ -22,6 +23,54 @@ namespace Assets._Progect.Develop.Runtime.Utillitles.StateMachineCore
             StateNode<TState> to = _states.First(stateNode => stateNode.State == toState);
 
             from.AddTransition(new StateTransition<TState>(to, condition));
+        }
+
+        public void Update(float deltaTime)
+        {
+            if(_isRunning == false)
+                return;
+
+            foreach (StateTransition<TState> transition in _currentState.Transitions)
+            {
+                if(transition.Condition.Evaluate())
+                {
+                    SwitchState(transition.ToState);
+                    break;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _isRunning = false;
+
+            foreach (StateNode<TState> stateNode in _states)
+                if(stateNode.State is IDisposable disposableState)
+                    disposableState.Dispose();
+
+            _states.Clear();
+        }
+
+        public void Enter()
+        {
+            if (_currentState == null)
+                SwitchState(_states[0]);
+
+            _isRunning = true;
+        }
+
+        public void Exsit()
+        {
+            _currentState?.State.Exit();
+
+            _isRunning = false;
+        }
+
+        private void SwitchState(StateNode<TState> nextState)
+        {
+            _currentState?.State.Exit();
+            _currentState = nextState;
+            _currentState.State.Enter();
         }
     }
 
