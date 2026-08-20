3b71c99e47b1bc1cf718560fca368c09039bd975
emcha6work-arch <emcha6work@gmail.com>
2026-08-20 04:38:14 +0300

    Added AIParallelState

---
+++ Assets/_Progect/Develop/Runtime/Gameplay/EntitiesCore/Features/AI/AIParallelState.cs
@@ -0,0 +1,18 @@
+﻿using Assets._Progect.Develop.Runtime.Utillitles.StateMachineCore;
+
+namespace Assets._Progect.Develop.Runtime.Gameplay.EntitiesCore.Features.AI
+{
+    public class AIParallelState : ParallelState<IUpdatableState>, IUpdatableState
+    {
+        public AIParallelState(params IUpdatableState[] states) : base(states)
+        {
+
+        }
+
+        public void Update(float deltaTime)
+        {
+            foreach (IUpdatableState state in States)
+                state.Update(deltaTime);
+        }
+    }
+}
---
+++ Assets/_Progect/Develop/Runtime/Gameplay/EntitiesCore/Features/AI/States/NearestDamageableTargetSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Progect.Develop.Runtime.Gameplay.EntitiesCore.Features.AI.States
+{
+    public class NearestDamageableTargetSelector : ITargetSelector
+    {
+        private Entity _sourse;
+        private Transform _sourseTransform;
+
+        public NearestDamageableTargetSelector(Entity entity)
+        {
+            _sourse = entity;
+            _sourseTransform = entity.Transform;
+        }
+
+        public Entity SelectTargetFrom(IEnumerable<Entity> targets)
+        {
+            IEnumerable<Entity> selectedTargets = targets.Where(target =>
+            {
+                return false;
+            });
+
+            return null;
+        }
+    }
+}
